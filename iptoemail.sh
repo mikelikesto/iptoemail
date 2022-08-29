6a36a21ea6fbcdabe70c2ff3eab640a39d21dbb6
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-29 12:21:16 +1000

    Update iptoemail.sh

--- iptoemail.sh
+++ iptoemail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 20
-echo -e 'Subject: test\n\n '"$(curl ifconfig.me)"'' | sendmail -v email@gmail.com
+echo -e 'Subject: IP\n\n '"$(curl ifconfig.me)"'' | sendmail -v email@gmail.com
