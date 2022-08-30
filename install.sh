eda8ffc00f77a0682184c66916efe8b0d1cef77f
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-30 12:49:05 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
  
-chmod +x iptoemail.sh && mv iptoemail.sh /home/$USER/.config && sudo gcc iptoemail.c -o iptoemail && sleep 3 && sudo mv iptoemail /bin
+chmod +x iptoemail.sh && sudo mv iptoemail.sh /bin && sudo gcc iptoemail.c -o iptoemail && sleep 3 && sudo mv iptoemail /bin
